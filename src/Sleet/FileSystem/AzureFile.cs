b0aa8aa4d0a4816395b5eb12b8572883d26232f9
Justin Emgarten <justin@emgarten.com>
2016-02-28 19:20:49 -0800

    Adding Azure filesystem

---
+++ src/Sleet/FileSystem/AzureFile.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NuGet.Logging;
+
+namespace Sleet
+{
+    public class AzureFile : ISleetFile
+    {
+        private readonly AzureFileSystem _fileSystem;
+        private readonly Uri _path;
+        private readonly FileInfo _localCacheFile;
+        private readonly CloudBlockBlob _blob;
+        private bool _isLoaded;
+
+        internal AzureFile(AzureFileSystem fileSystem, Uri path, FileInfo localCacheFile, CloudBlockBlob blob)
+        {
+            _fileSystem = fileSystem;
+            _path = path;
+            _localCacheFile = localCacheFile;
+            _blob = blob;
+        }
+
+        public ISleetFileSystem FileSystem
+        {
+            get
+            {
+                return _fileSystem;
+            }
+        }
+
+        public Uri Path
+        {
+            get
+            {
+                return _path;
+            }
+        }
+
+        public async Task<bool> Exists(ILogger log, CancellationToken token)
+        {
+            var file = await GetLocal(log, token);
+            return file.Exists;
+        }
+
+        public async Task Get(ILogger log, CancellationToken token)
+        {
+            if (!_isLoaded)
+            {
+                if (await _blob.ExistsAsync())
+                {
+                    log.LogInformation($"GET {_blob.Uri.AbsoluteUri}");
+
+                    if (File.Exists(_localCacheFile.FullName))
+                    {
+                        _localCacheFile.Delete();
+                    }
+
+                    using (var cache = _localCacheFile.OpenWrite())
+                    {
+                        await _blob.DownloadToStreamAsync(cache);
+                    }
+                }
+
+                _isLoaded = true;
+            }
+        }
+
+        public async Task<FileInfo> GetLocal(ILogger log, CancellationToken token)
+        {
+            await Get(log, token);
+            return _localCacheFile;
+        }
+
+        public async Task Push(ILogger log, CancellationToken token)
+        {
+            if (File.Exists(_localCacheFile.FullName))
+            {
+                log.LogInformation($"Pushing {_blob.Uri.AbsoluteUri}");
+
+                using (var cache = _localCacheFile.OpenRead())
+                {
+                    await _blob.UploadFromStreamAsync(cache);
+                    _blob.Properties.CacheControl = "no-store";
+                    await _blob.SetPropertiesAsync();
+                }
+            }
+            else if (await _blob.ExistsAsync())
+            {
+                log.LogInformation($"Removing {_blob.Uri.AbsoluteUri}");
+                await _blob.DeleteAsync();
+            }
+            else
+            {
+                log.LogInformation($"Skipping {_blob.Uri.AbsoluteUri}");
+            }
+        }
+    }
+}
---
+++ src/Sleet/FileSystem/AzureFileSystem.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NuGet.Logging;
+
+namespace Sleet
+{
+    public class AzureFileSystem : ISleetFileSystem
+    {
+        private readonly Uri _root;
+        private readonly LocalCache _cache;
+        private readonly ConcurrentDictionary<Uri, ISleetFile> _files;
+        private readonly CloudStorageAccount _azureAccount;
+        private readonly CloudBlobClient _client;
+        private readonly CloudBlobContainer _container;
+
+        public AzureFileSystem(LocalCache cache, Uri root, CloudStorageAccount azureAccount)
+        {
+            _root = new Uri(root.AbsoluteUri.TrimEnd('/') + '/');
+            _cache = cache;
+            _files = new ConcurrentDictionary<Uri, ISleetFile>();
+
+            // Use the directory of the root url as the container name
+            var containerName = _root.AbsoluteUri.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries).Last();
+
+            _azureAccount = azureAccount;
+            _client = _azureAccount.CreateCloudBlobClient();
+            _container = _client.GetContainerReference(containerName);
+        }
+
+        public ConcurrentDictionary<Uri, ISleetFile> Files
+        {
+            get
+            {
+                return _files;
+            }
+        }
+
+        public LocalCache LocalCache
+        {
+            get
+            {
+                return _cache;
+            }
+        }
+
+        public Uri Root
+        {
+            get
+            {
+                return _root;
+            }
+        }
+
+        public ISleetFile Get(string relativePath)
+        {
+            return Get(GetPath(relativePath));
+        }
+
+        public ISleetFile Get(Uri path)
+        {
+            var relativePath = GetRelativePath(path);
+
+            var blob = _container.GetBlockBlobReference(relativePath);
+
+            var file = Files.GetOrAdd(path, (uri) => new AzureFile(
+                this,
+                uri,
+                LocalCache.GetNewTempPath(),
+                blob));
+
+            return file;
+        }
+
+        public Uri GetPath(string relativePath)
+        {
+            var combined = new Uri(Root, relativePath);
+            return combined;
+        }
+
+        public async Task<bool> Commit(ILogger log, CancellationToken token)
+        {
+            foreach (var file in Files.Values)
+            {
+                await file.Push(log, token);
+            }
+
+            return true;
+        }
+
+        public string GetRelativePath(Uri uri)
+        {
+            return uri.AbsoluteUri.Replace(_root.AbsoluteUri, string.Empty);
+        }
+    }
+}
