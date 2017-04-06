2b8f4ab38d3cc0b918d858112018e970f5361d65
Justin Emgarten <justin@emgarten.com>
2017-04-05 20:40:06 -0700

    Removing -v shortcut for --verbose due to conflicts with --version

--- src/Sleet/Constants.cs
+++ src/Sleet/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sleet
+﻿namespace Sleet
 {
     internal class Constants
     {
@@ -11,7 +7,7 @@ namespace Sleet
         internal const string ConfigDesc = "sleet.json file to read sources and settings from.";
         internal const string SourceOption = "-s|--source";
         internal const string SourceDesc = "Source name from sleet.json.";
-        internal const string VerboseOption = "-v|--verbose";
+        internal const string VerboseOption = "--verbose";
         internal const string VerboseDesc = "Write out additional messages for verbose output.";
         internal const string DisableCatalogOption = "--disable-catalog";
         internal const string DisableCatalogDesc = "Disable the feed catalog and all change history tracking. This is useful for feeds that frequently replace or remove packages.";
