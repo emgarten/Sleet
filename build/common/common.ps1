07c8e26b56be70215ff25c5d9b3fbcb9319065e0
Justin Emgarten <justin@emgarten.com>
2018-04-08 21:32:59 -0700

    Move to global DotnetTool package (#32)
    Move to global DotnetTool package
    
    Fixes https://github.com/emgarten/Sleet/issues/31

--- build/common/common.ps1
+++ build/common/common.ps1
@@ -20,8 +20,9 @@ Function Install-DotnetCLI {
 
         Write-Host "Fetching $installDotnet"
 
-        wget https://raw.githubusercontent.com/dotnet/cli/4bd9bb92cc3636421cd01baedbd8ef3e41aa1e22/scripts/obtain/dotnet-install.ps1 -OutFile $installDotnet
+        wget https://raw.githubusercontent.com/dotnet/cli/62ce305f06c502a2fae459fc906c982b943c6128/scripts/obtain/dotnet-install.ps1 -OutFile $installDotnet
 
+        & $installDotnet -i $CLIRoot -Version 2.1.300-preview2-008523
         & $installDotnet -Channel 2.0 -i $CLIRoot -Version 2.0.3
         & $installDotnet -Channel preview -i $CLIRoot -Version 1.0.4
 
