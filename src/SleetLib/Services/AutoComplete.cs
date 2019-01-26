34280d1d33011e94d587f5b9439901e0bd8d2962
Justin Emgarten <justin@emgarten.com>
2019-01-26 13:02:47 -0800

    Remove unused methods from AutoComplete

--- src/SleetLib/Services/AutoComplete.cs
+++ src/SleetLib/Services/AutoComplete.cs
@@ -27,16 +27,6 @@ namespace Sleet
 
         public ISleetFile RootIndexFile => _context.Source.Get(RootIndex);
 
-        public Task AddPackageAsync(PackageInput packageInput)
-        {
-            return AddPackagesAsync(new[] { packageInput });
-        }
-
-        public Task RemovePackageAsync(PackageIdentity packageIdentity)
-        {
-            return RemovePackagesAsync(new[] { packageIdentity });
-        }
-
         /// <summary>
         /// Returns all known ids.
         /// </summary>
@@ -70,58 +60,6 @@ namespace Sleet
 
             var formatted = JsonLDTokenComparer.Format(json, recurse: false);
             await RootIndexFile.Write(formatted, _context.Log, _context.Token);
-        }
-
-        // TODO: Remove this
-        public async Task AddPackagesAsync(IEnumerable<PackageInput> packageInputs)
-        {
-            var ids = await GetPackageIds();
-            var prevCount = ids.Count;
-            ids.UnionWith(packageInputs.Select(e => e.Identity.Id));
-
-            // Only update the file if needed.
-            if (prevCount != ids.Count)
-            {
-                var file = RootIndexFile;
-                var json = await file.GetJson(_context.Log, _context.Token);
-                var data = json["data"] as JArray;
-                data.Clear();
-
-                foreach (var id in ids.OrderBy(s => s, StringComparer.OrdinalIgnoreCase))
-                {
-                    data.Add(id);
-                }
-
-                json["totalHits"] = ids.Count;
-                await file.Write(json, _context.Log, _context.Token);
-            }
-        }
-
-        // TODO: Remove this
-        public async Task RemovePackagesAsync(IEnumerable<PackageIdentity> packages)
-        {
-            var packageIndex = new PackageIndex(_context);
-            var allPackages = await packageIndex.GetPackagesAsync();
-            var after = new HashSet<PackageIdentity>(allPackages.Except(packages));
-            var allPackagesIds = new HashSet<string>(allPackages.Select(e => e.Id), StringComparer.OrdinalIgnoreCase);
-            var afterIds = new HashSet<string>(after.Select(e => e.Id), StringComparer.OrdinalIgnoreCase);
-
-            // Only update if needed
-            if (allPackagesIds.Count != afterIds.Count)
-            {
-                var file = RootIndexFile;
-                var json = await file.GetJson(_context.Log, _context.Token);
-                var data = json["data"] as JArray;
-                data.Clear();
-
-                foreach (var id in afterIds.OrderBy(s => s, StringComparer.OrdinalIgnoreCase))
-                {
-                    data.Add(id);
-                }
-
-                json["totalHits"] = after.Count;
-                await file.Write(json, _context.Log, _context.Token);
-            }
         }
 
         private async Task<JObject> GetEmptyTemplate()
