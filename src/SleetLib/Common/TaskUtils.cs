7e3e043caa35d9af6d6bb5a97d6afc7b1dd45da9
Martin Finkel <me@martinfinkel.com>
2025-07-06 20:45:57 +0700

    TaskUtils: use provided parameter

--- src/SleetLib/Common/TaskUtils.cs
+++ src/SleetLib/Common/TaskUtils.cs
@@ -55,7 +55,7 @@ namespace Sleet
         {
             return RunAsync(
                 tasks: tasks.Select(GetFuncWithReturnValue),
-                useTaskRun: false,
+                useTaskRun: useTaskRun,
                 token: token);
         }
 
@@ -66,7 +66,7 @@ namespace Sleet
         {
             return RunAsync(
                 tasks: tasks.Select(GetFuncWithReturnValue),
-                useTaskRun: false,
+                useTaskRun: useTaskRun,
                 maxThreads: maxThreads,
                 token: token);
         }
