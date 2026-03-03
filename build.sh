a34287e6197050dca9f5e7d9662e90e091dc0657
Justin Emgarten <justin@emgarten.com>
2026-03-02 23:23:23 -0800

    Fix getopt

--- build.sh
+++ build.sh
@@ -3,31 +3,33 @@ set -e
 
 # No options are needed to run a basic build and unit tests
 # To run functional tests against azure and or aws, use the following options:
-VALID_ARGS=$(getopt -o : --long azure-conn:,aws-key:,aws-secret:,aws-region: -- "$@")
-if [[ $? -ne 0 ]]; then
-    exit 1;
-fi
-
-eval set -- "$VALID_ARGS"
-while [ : ]; do
+while [[ $# -gt 0 ]]; do
   case "$1" in
     --azure-conn)
-        export SLEET_TEST_ACCOUNT=$2
+        export SLEET_TEST_ACCOUNT="$2"
         shift 2
         ;;
     --aws-key)
-        export AWS_ACCESS_KEY_ID=$2
+        export AWS_ACCESS_KEY_ID="$2"
         shift 2
         ;;
     --aws-secret)
-        export AWS_SECRET_ACCESS_KEY=$2
+        export AWS_SECRET_ACCESS_KEY="$2"
         shift 2
         ;;
     --aws-region)
-        export AWS_DEFAULT_REGION=$2
+        export AWS_DEFAULT_REGION="$2"
         shift 2
         ;;
-    --) shift;
+    --)
+        shift
+        break
+        ;;
+    --*)
+        echo "Unknown option: $1" >&2
+        exit 1
+        ;;
+    *)
         break
         ;;
   esac
